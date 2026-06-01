b64a46200637bad81d4f33a792ce6808e7c760d7
Stepanzem <stepan.zemlicka.025@pslib.cz>
2026-06-01 10:57:56 +0200

    first prototype script added

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
+radio.setGroup(23)
+radio.setTransmitSerialNumber(true)
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(control.deviceSerialNumber())
+})
 
+while (true) {
+    let pressedA = input.buttonIsPressed(Button.A)
+    let pressedB = input.buttonIsPressed(Button.B)
+    if (pressedA && pressedB) {
+
+        radio.sendString("goForward")
+    }
+    else if (pressedB) {
+        radio.sendString("turnRight")
+    }
+    else if (pressedA ) {
+        radio.sendString("turnLeft")
+    }
+    else {
+        radio.sendString("stop")
+    }
+    basic.pause(50)
+}
