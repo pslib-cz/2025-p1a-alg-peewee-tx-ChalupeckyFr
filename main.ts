20971ca064d2f65fdc5342ecbef649666283c80b
ChalupeckyFr <frantisek.chal.025@pslib.cz>
2026-06-08 10:55:14 +0200

    added functional převodovka

--- main.ts
+++ main.ts
@@ -1,22 +1,28 @@
-//**ovladac**
+let pressedLogo = false
+let pressedB = false
+let pressedA = false
+let shaked = false
+
+
+// **Ovladač**
 radio.setGroup(23)
 radio.setTransmitSerialNumber(true)
-
-basic.forever(function(){
-    let pressedA = input.buttonIsPressed(Button.A)
-    let pressedB = input.buttonIsPressed(Button.B)
-    let pressedLogo = input.logoIsPressed()
+basic.forever(function () {
+    pressedA = input.buttonIsPressed(Button.A)
+    pressedB = input.buttonIsPressed(Button.B)
+    pressedLogo = input.logoIsPressed()
+    shaked = input.isGesture(Gesture.Shake)
     if (pressedA && pressedB) {
         radio.sendString("goForward")
-    } 
-   else if(pressedLogo){
-       radio.sendString("goBack")
-   }
-    else if (pressedA) {
+    } else if (pressedLogo) {
+        radio.sendString("goBack")
+    } else if (pressedA) {
         radio.sendString("turnLeft")
     } else if (pressedB) {
         radio.sendString("turnRight")
+    } else if (shaked) {
+        radio.sendString("shaked")
     } else {
         radio.sendString("stop")
-    }    
+    }
 })
